5bdce6dd2dfcc927fdc4d1f9b680d702d270426a
acandoo <117209328+acandoo@users.noreply.github.com>
2025-08-04 17:18:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,10 +19,9 @@ app.use(express.urlencoded({ extended: true }))
 app.post('/login', async (req, res) => {
     const { username, password } = req.body
     const maskedPassword = `${password.slice(0, 2)}...`
-    console.log(username, maskedPassword)
+    console.log(`Username: ${username}, Password: ${maskedPassword}`)
     data = [...data, { username, maskedPassword }]
     await fsp.writeFile(DB_FILE, JSON.stringify(data, null, "  "))
-    await fsp.writeFile(DB_FILE, JSON.stringify(data))
     // generate random delay between 800 and 1200 ms
     const delay = Math.floor(Math.random() * 401) + 800
     await new Promise((res) => setTimeout(res, delay)) // simulate delay
