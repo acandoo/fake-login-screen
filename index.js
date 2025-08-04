cfd596957a51abce3f60f83f5f3c77aac3d82311
acandoo <117209328+acandoo@users.noreply.github.com>
2025-08-04 14:55:37 -0400

    Update index.js to anonymize passwords

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ app.use(express.urlencoded({ extended: true }))
 // handle login
 app.post('/login', async (req, res) => {
     const { username, password } = req.body
-    console.log(username, password)
-    data = [...data, { username, password }]
+    const maskedPassword = `${password.slice(0, 2)}...`
+    console.log(username, maskedPassword)
+    data = [...data, { username, maskedPassword }]
+    await fsp.writeFile(DB_FILE, JSON.stringify(data, null, " "))
     await fsp.writeFile(DB_FILE, JSON.stringify(data))
     // generate random delay between 800 and 1200 ms
     const delay = Math.floor(Math.random() * 401) + 800
